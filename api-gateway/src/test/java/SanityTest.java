29ed605c2739d009503ddeb147ae11c2b6c3e37c
leelamanisankarpeerukattla <leelamanisankarpeerukattla@gmail.com>
2026-01-12 18:59:02 -0500

    Fix CI: exclude datasource/jpa/flyway autoconfig in gateway test

--- api-gateway/src/test/java/SanityTest.java
+++ api-gateway/src/test/java/SanityTest.java
@@ -2,11 +2,19 @@ import com.example.gateway.ApiGatewayApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = ApiGatewayApplication.class)
+@SpringBootTest(
+        classes = ApiGatewayApplication.class,
+        properties = {
+                "spring.autoconfigure.exclude=" +
+                        "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
+                        "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration," +
+                        "org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration"
+        }
+)
 class SanityTest {
 
   @Test
   void contextLoads() {
-    // verifies Spring context can start
+    // context load check
   }
 }
