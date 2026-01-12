fad29d7565bb57ff37a7935964d0b087fe1b1f72
leelamanisankarpeerukattla <leelamanisankarpeerukattla@gmail.com>
2026-01-12 18:48:26 -0500

    "Fix gateway CI test: specify SpringBoot Applicationclass"

--- api-gateway/src/test/java/SanityTest.java
+++ api-gateway/src/test/java/SanityTest.java
@@ -1,7 +1,12 @@
+import com.example.gateway.ApiGatewayApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ApiGatewayApplication.class)
 class SanityTest {
-  @Test void contextLoads() {}
+
+  @Test
+  void contextLoads() {
+    // verifies Spring context can start
+  }
 }
